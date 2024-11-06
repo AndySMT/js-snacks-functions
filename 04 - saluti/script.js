8714493bcdf1745165dde96c83eecc09fc52993e
Andy Simota <andyiulian.simota@gmail.com>
2024-11-06 18:24:45 +0100

    Saluti

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -1,13 +1,17 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
-const name = 'Mario';
-
+const name = "Mario";
 
 // Dichiara la funzione qui.
-
+function hello(nome) {
+  return "ciao " + nome;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
-
+console.log(hello(name));
 //Risultato atteso se si passa 'Mario': // ciao Mario
+
+/* ArrowFunction */
+
+let helloArrow = (nome) => "ciao " + nome;
+console.log(helloArrow(name));
