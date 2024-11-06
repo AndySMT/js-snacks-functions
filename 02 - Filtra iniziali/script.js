6104c78c676be8f311f4d767e9f2484a1ed5aa48
Andy Simota <andyiulian.simota@gmail.com>
2024-11-06 17:05:23 +0100

    Fixed iniziali

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -3,28 +3,28 @@
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Dichiara la funzione qui.
-function specificChar(nameList) {
+function specificChar(nameList, character) {
   let firstChar = [];
-  for (let i = 0; i <= names.length - 1; i++) {
-    if (names[i].charAt(0) === "A") {
-      firstChar.push(names[i]);
+  for (let i = 0; i <= nameList.length - 1; i++) {
+    if (nameList[i].charAt(0) === character) {
+      firstChar.push(nameList[i]);
     }
   }
   return firstChar;
 }
 
 // Invoca la funzione qui e stampa il risultato in console
-let specific = specificChar(names);
+let specific = specificChar(names, "A");
 console.log(specific);
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
 
-let specificCharArrow = (nameList) => {
+let specificCharArrow = (nameList, character) => {
   let firstChar = [];
-  for (let i = 0; i <= names.length - 1; i++) {
-    if (names[i].charAt(0) === "A") {
+  for (let i = 0; i <= nameList.length - 1; i++) {
+    if (nameList[i].charAt(0) === character) {
       firstChar.push(names[i]);
     }
   }
   return firstChar;
 };
-console.log(specificCharArrow(names));
+console.log(specificCharArrow(names, "L"));
